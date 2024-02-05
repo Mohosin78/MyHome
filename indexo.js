27a542515f887354188c58b28ae8a6f81cfb6a37
Md.Mohosin Ali <74634015+Mohosin78@users.noreply.github.com>
2024-02-05 22:50:12 +0600

    Update indexo.js

--- indexo.js
+++ indexo.js
@@ -1,17 +1,28 @@
-const WebSocket = require('ws');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const wss = new WebSocket.Server({ port: 443 });
+app.use(express.static('public'));
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    // Handle incoming signaling messages
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    io.emit("Welcome");
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
-  });
+
+    socket.on('chat', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
+const PORT = process.env.PORT || 3000;
 
-console.log("Running Web Socket");
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+});
